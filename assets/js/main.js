7a391e5e8d0ab1d2c8dab3b0fef07523f9db2105
mahdul-00 <mdmahdul10@gmail.com>
2025-08-20 03:38:09 -0700

    Header Section End

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    "use strict";
+
+    // color
+    $("[data-color]").each(function () {
+        $(this).css("color", $(this).attr("data-color"))
+    })
+
+    // bg color
+    $("[data-bg-color]").each(function () {
+        $(this).css("background-color", $(this).attr("data-bg-color"))
+    })
+    
+
+})(jQuery);

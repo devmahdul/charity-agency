5fb85d3958ad7963528d0b87e9c7da4759a4928f
Mahdul Hasan <mdmahdul10@gmail.com>
2025-11-10 01:49:43 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -285,7 +285,7 @@
             var wow = new WOW({
                 boxClass: 'wow',
                 animateClass: 'animated',
-                offset: 0,
+                offset: 100,
                 mobile: true,
                 live: true
             });
